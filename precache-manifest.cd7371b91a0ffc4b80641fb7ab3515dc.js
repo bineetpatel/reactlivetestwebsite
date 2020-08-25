be189512a451f13353b0c4cacc6aa95bd0b70b49
bineet Patel <binwithu@gmail.com>
2020-08-26 00:52:01 +0530

    Updates

---
+++ precache-manifest.cd7371b91a0ffc4b80641fb7ab3515dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ca326620cf1b077580ec34aa04bc3c",
+    "url": "/reactlivetestwebsite/index.html"
+  },
+  {
+    "revision": "f6e0e04189c929f4434e",
+    "url": "/reactlivetestwebsite/static/css/main.3f22b8cc.chunk.css"
+  },
+  {
+    "revision": "dfbe1a311b0830d01dca",
+    "url": "/reactlivetestwebsite/static/js/2.be51b0be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactlivetestwebsite/static/js/2.be51b0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e0e04189c929f4434e",
+    "url": "/reactlivetestwebsite/static/js/main.53d5a0bc.chunk.js"
+  },
+  {
+    "revision": "65e81bc1a9402fb87962",
+    "url": "/reactlivetestwebsite/static/js/runtime-main.56e64a0e.js"
+  }
+]);
